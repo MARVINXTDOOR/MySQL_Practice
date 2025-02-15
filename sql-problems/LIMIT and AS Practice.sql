587830f3dfc9b264fad1ad0131b6f100ea9b68b5
Marvin Pearson <marvinbpearson@students.abtech.edu>
2025-02-15 15:37:05 -0800

    Add files via upload

---
+++ sql-problems/LIMIT and AS Practice.sql
@@ -0,0 +1,13 @@
+-- LIMIT & ALIASING
+
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 2, 1;
+
+-- ALIASING
+
+SELECT gender, AVG(age) AS avg_age
+FROM employee_demographics
+GROUP BY gender
+HAVING AVG(age) > 40
