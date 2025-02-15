cd8a1458ec3034f9eb73ab09e8f156ad4aac0c5c
Marvin Pearson <marvinbpearson@students.abtech.edu>
2025-02-15 15:38:23 -0800

    Add files via upload

---
+++ MySQL Training Done/HAVING Statement Practice.sql
@@ -0,0 +1,13 @@
+-- HAVING vs WHERE
+SELECT gender, AVG (age)
+FROM employee_demographics
+GROUP BY gender
+HAVING avg(age) > 40;
+
+
+
+SELECT occupation, AVG(salary)
+FROM employee_salary
+WHERE occupation LIKE '%manager%'
+GROUP BY occupation 
+HAVING AVG(salary) > 75000
